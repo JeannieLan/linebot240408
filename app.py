6a408e81f9dcbe0c1339d65b34aba3937e6b0f85
JeannieLan <117909693+JeannieLan@users.noreply.github.com>
2024-04-08 22:41:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -45,7 +45,8 @@ def send_mqtttoline(message):
         print("Failed to send message to Line:", e)
 
 # 創建 MQTT 客戶端
-mqtt_client = mqtt.Client()
+mqtt_client = mqtt.Client(callback_api_version=mqtt.MQTTv311)  # 或者使用 MQTTv5
+
 
 # 設置連接後的回調函數
 mqtt_client.on_connect = on_connect
